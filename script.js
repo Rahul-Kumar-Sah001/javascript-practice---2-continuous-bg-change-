e39be284e20c0c0cdb3b1ae1ac722118659070e9
Rahul <rahulsah7846@gmail.com>
2024-08-22 12:30:17 +0530

    "little modification"

--- script.js
+++ script.js
@@ -1,23 +1,28 @@
 const start = document.querySelector('.start');
 const stop = document.querySelector('.stop');
 
-const randomColor = function(){
+const randomColor = function () {
     let hex = '0123456789ABCDEF';
-    let color ="#"; 
-    for(let i = 0;i<6;i++){
-        color+=hex[Math.floor(Math.random()*16)];
+    let color = "#";
+    for (let i = 0; i < 6; i++) {
+        color += hex[Math.floor(Math.random() * 16)];
     }
     return color;
 }
-const changeBackground = function(){
+const changeBackground = function () {
     document.body.style.backgroundColor = randomColor();
 }
 
 let changeColor;
-start.addEventListener('click',()=>{
-    changeColor = setInterval(changeBackground,1000);
-});
-
-stop.addEventListener('click',()=>{
-   if(changeColor) clearInterval(changeColor);
-});
+const startChangingColor = () => {
+    if (!changeColor) changeColor = setInterval(changeBackground, 1000);
+}
+const stopChangingColor =
+    () => {
+        if (changeColor) {
+            clearInterval(changeColor);
+            changeColor = null;
+        }
+    }
+start.addEventListener('click', startChangingColor);
+stop.addEventListener('click', stopChangingColor);
